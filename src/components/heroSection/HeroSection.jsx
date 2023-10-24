d2dbfa4096ff591a5963a2c83a0fa4e3915fcb8e
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2023-10-24 14:51:34 +0530

    create hero section

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import styles from "./HeroSection.module.scss";
 
 const HeroSection = () => {
-  return <div className={styles.hero_section_container}>HeroSection</div>;
+  return (
+    <div className={styles.hero_section_container}>
+      <span>Marketplace</span>
+      <div>
+        <h1 className={styles.heading1}>Email Templates</h1>
+        <h1 className={styles.heading2}>
+          by the community, for the community.
+        </h1>
+      </div>
+      <p className={styles.text_info}>
+        100s of free templates to help you craft the perfect marketing journey.
+      </p>
+    </div>
+  );
 };
 
 export default HeroSection;

e0068423a7486526a9e0b0f19f4a6e55c40d8068
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2023-10-24 14:28:51 +0530

    feat: complete category checkbox

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Sidebar.module.scss";
 
 const Sidebar = () => {
-  return <div className={styles.sidebar}>Sidebar</div>;
+  const [selectCategory, setSelectCategory] = useState([]);
+
+  const handeChange = (e) => {
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+      setSelectCategory([...selectCategory, value]);
+    } else {
+      setSelectCategory(
+        selectCategory.filter((filteredValue) => filteredValue !== value)
+      );
+    }
+  };
+
+  return (
+    <div className={styles.sidebar}>
+      <span>Categories</span>
+      {["Announcement", "Educate & Inform ", "Invitation", "Ocassions"].map(
+        (category) => (
+          <div className={styles.checkbox}>
+            <input
+              type="checkbox"
+              value={category}
+              id={category}
+              checked={selectCategory.includes(category)}
+              onChange={handeChange}
+            />
+            <label htmlFor={category} key={category}>
+              {category}
+            </label>
+          </div>
+        )
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
